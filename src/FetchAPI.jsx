3d123e1c9661a183b56f47dcf30ccf03ed37d82b
Issa-WebDev <kissadev10@gmail.com>
2025-03-28 23:43:27 +0100

    increase the number of results to 50

--- src/FetchAPI.jsx
+++ src/FetchAPI.jsx
@@ -19,7 +19,7 @@ const FetchDataProvider = ({ children }) => {
     const endpoint = encodeURIComponent(query);
     try {
       const response = await fetch(
-        `${apiUrl}?key=${apiKey}&q=${endpoint}&image_type=photo&per_page=21`
+        `${apiUrl}?key=${apiKey}&q=${endpoint}&image_type=photo&per_page=50`
       );
       const data = await response.json();
       if (data && data.hits.length > 0) {
